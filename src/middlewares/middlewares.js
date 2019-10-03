e0748bb9ef6c9584ee75499b2b15f76f5c43d63b
kimtaeho <xxxxoghxxx@naver.com>
2019-10-03 20:59:59 +0900

    #build SCSS

--- src/middlewares/middlewares.js
+++ src/middlewares/middlewares.js
@@ -7,7 +7,7 @@ export const localMiddleware = (req, res, next) => {
     res.locals.siteName = "WeTube";
     res.locals.routes = routes;
     res.locals.user={
-        isAuthenticated: true,
+        isAuthenticated: false,
         id: 1
     }
     next(); // 이 경우에 localMiddleware선언한 지점이 connection과 router 사이라서
